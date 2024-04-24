051b7542951bf3558e1fc3ac85bf561cbaf73e25
1324365 <165462590+1324365@users.noreply.github.com>
2024-04-24 18:50:18 +0100

    Update corrbyday.m

--- corrbyday.m
+++ corrbyday.m
@@ -1,6 +1,6 @@
 % Define folder paths for the two sets of data
-folderPath1 = '/Users/laurahurn/Desktop/diss/data/active /gabs/mw 180'; % Update with the actual folder path
-folderPath2 = '/Users/laurahurn/Desktop/diss/data/active /gabs/t'; % Update with the actual folder path
+folderPath1 = ''; % Update with the actual folder path
+folderPath2 = ''; % Update with the actual folder path
 
 % Create a matrix to store correlation coefficients for each day
 correlationMatrix = NaN(10, 1);
@@ -8,8 +8,8 @@ correlationMatrix = NaN(10, 1);
 % Loop through each day
 for day = 1:10
     % Construct file paths for the two sets of data for the current day
-    fileName1 = sprintf('mw_gab_180_%03d.csv', day); % Assuming file names are in the format per_tetgab180_001.csv, per_tetgab180_002.csv, etc.
-    fileName2 = sprintf('interpolated_response_time180_%d.csv', day); % Assuming file names are in the format interpolated_response_time180_1.csv, interpolated_response_time180_2.csv, etc.
+    fileName1 = sprintf('', day); 
+    fileName2 = sprintf('', day); 
     filePath1 = fullfile(folderPath1, fileName1);
     filePath2 = fullfile(folderPath2, fileName2);
     
@@ -30,12 +30,12 @@ for day = 1:10
 end
 
 % Save the correlation matrix to a CSV file
-outputFilePath = '/Users/laurahurn/Desktop/diss/data/active /gabs/mwxRTcorrelation_results.csv';
+outputFilePath = '';
 csvwrite(outputFilePath, correlationMatrix);
 
 % Plot the correlations
 figure;
-plot(1:10, correlationMatrix, '-o'); % Changed from 1:11 to 1:10 as there are 10 days
+plot(1:10, correlationMatrix, '-o'); 
 xlabel('Day');
 ylabel('Correlation Coefficient');
 title('Correlation between per and response time for each pair of days');
