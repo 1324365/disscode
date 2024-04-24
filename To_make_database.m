9c6c816c74645177ec75572925578983e4addd7b
1324365 <165462590+1324365@users.noreply.github.com>
2024-04-24 18:48:21 +0100

    Update To_make_database.m

--- To_make_database.m
+++ To_make_database.m
@@ -4,12 +4,12 @@
 
 for i = 1:300
 
-ODESA_APP_DATA(i).DailyTrace =struct('Time', [],'Wakefulness', [], 'Emotions', [], 'Anxiety', [], 'Busyness', [], 'Attention', []);
+ODESA_APP_DATA(i).DailyTrace =struct();
 
-ODESA_APP_DATA(i).DriveTrace =struct('Time', [],'Performance', [], 'Wakefulness', [], 'Effort', [], 'Emotions', [], 'Rumination', [], 'Mind', []);
+ODESA_APP_DATA(i).activetask =struct();
 
-ODESA_APP_DATA(i).DriveQ =struct('Time', [],'Regular', [], 'Passengers', [], 'Listen', [], 'Affect', [], 'Attention', [], 'How_Long', [], 'Purpose', []);
+ODESA_APP_DATA(i).passivetask =struct();
 
-ODESA_APP_DATA(i).DailyQ =struct('Time', [],'Into_Bed', [], 'Out_Bed', [], 'CPAP', [], 'Sleep_Rating',[]);
+ODESA_APP_DATA(i).pretest =struct(');
 
 end 
