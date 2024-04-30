6ec4f477f076f4e99f6e55254c78aaf987ad45ca
1324365 <165462590+1324365@users.noreply.github.com>
2024-04-30 12:14:43 +0100

    Update TET_Interpolation.m

--- TET_Interpolation.m
+++ TET_Interpolation.m
@@ -1,13 +1,13 @@
 
-mw = []; % Initialize all as an empty array
+mw = [];
 
 for i = 1:x
-    n_samples =x; % Set n_samples to x within the loop
+    n_samples =x;
 
-    % Assuming `data` is the same as `Ex`
+   
     Ex = ODESA_APP_DATA(x).activetask(i).performance
 
-    % Assuming `Ex` is a matrix where each column represents a variable
+    
     d = Ex(:, 2); % Extracting column 2 of Ex
 
     interp_meth = 'spline'; % Interpolation method
@@ -15,20 +15,20 @@ for i = 1:x
     idx = 1:length(d);
     int_vec = linspace(min(idx), max(idx), n_samples);
 
-    % Perform spline interpolation on column 2 of Ex
+   
     d_res = interp1(idx, d, int_vec, interp_meth);
 
-    % Transpose to vertical
+
     d_res = d_res.'; 
 
-    % Store interpolated values
+   
     mw = [mw; d_res];
 end
 
 
 csv_file_name = '';
 
-% Write the matrix to the CSV file
+
 csvwrite(csv_file_name, mw);
 
 
@@ -41,10 +41,10 @@ for i = 1:x
     % Assuming `data` is the same as `Ex`
     Ex = ODESA_APP_DATA(x).activetask(i).mindwandering
 
-    % Assuming `Ex` is a matrix where each column represents a variable
+
     d = Ex(:, 2); % Extracting column 2 of Ex
 
-    interp_meth = 'spline'; % Interpolation method
+    interp_meth = 'spline'; 
 
     idx = 1:length(d);
     int_vec = linspace(min(idx), max(idx), n_samples);
