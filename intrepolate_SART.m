e55b4cb46e65fea9dec2dc9f9835cc22dea35fbe
1324365 <165462590+1324365@users.noreply.github.com>
2024-04-30 12:19:23 +0100

    Update intrepolate_SART.m

--- intrepolate_SART.m
+++ intrepolate_SART.m
@@ -1,14 +1,14 @@
-per = []; % Initialize all as an empty array
+per = [];
 
 n_samples = 1440; % Change n_samples to 1980 within the loop
 
-% Load the new data source (assuming it's stored in a CSV file)
+
 data = readmatrix('', 'NumHeaderLines', 1); % Skip the first line (header)
 
-% Extract response times from column 10
-response_times = data(:, 10);
+% Extract response times from column x
+response_times = data(:, x);
 
-% Check for NaN values and remove them
+
 response_times = response_times(~isnan(response_times));
 
 % Interpolation method
@@ -20,13 +20,12 @@ if numel(response_times) >= 2 % Ensure enough sample points for interpolation
     int_vec = linspace(min(idx), max(idx), n_samples);
     interpolated_response_times = interp1(idx, response_times, int_vec, interp_method);
 
-    % Transpose to vertical
     interpolated_response_times = interpolated_response_times';
 
     % Store interpolated values
     per = [per; interpolated_response_times];
     
-    % Write the matrix to the CSV file
+   
     csv_file_name = '';
     writematrix(per, csv_file_name);
 else
